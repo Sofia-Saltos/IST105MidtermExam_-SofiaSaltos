2e4ed36c237c8160acfecb4fb0b6b4da9d870977
Sofia <s.sofiasaltos@gmail.com>
2024-11-02 15:28:34 -0700

    added files

---
+++ public/js/firebase.js
@@ -0,0 +1,25 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAI2wrdxpUzDtLhKcrtWst_wEu2kvT6iR8",
+  authDomain: "ist105midtermexam-sofiasaltos.firebaseapp.com",
+  projectId: "ist105midtermexam-sofiasaltos",
+  storageBucket: "ist105midtermexam-sofiasaltos.appspot.com",
+  messagingSenderId: "141108808958",
+  appId: "1:141108808958:web:ef5b5aca0ea99a7f62e9af"
+};
+  
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  
+  
